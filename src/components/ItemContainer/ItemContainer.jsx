4238204396eec2b7580829211737f823d7566e2f
Mitchenya <Mitchellrjshepherd@outlook.com>
2024-09-12 18:49:27 +0100

    This commit will ensure that no duplicates can be posted via the ItemContainer

--- src/components/ItemContainer/ItemContainer.jsx
+++ src/components/ItemContainer/ItemContainer.jsx
@@ -1,30 +1,40 @@
 import { useEffect, useState } from "react";
 import { getItems } from "../../utils/api";
-import "./ItemContainer.css"
+import "./ItemContainer.css";
 
-function ItemContainer(){
-const [ItemContainer, setItemContainer] = useState([])
-useEffect(() => {
-    getItems().then((data)=> {
-        
-        return setItemContainer(data)
-    })
-    },[])
+function ItemContainer() {
+  const [items, setItems] = useState([]);
 
-    return (
+  useEffect(() => {
+    getItems().then((data) => {
+      const seen = new Set();
+      const uniqueItems = data
+        .filter((item) => {
+          const isDuplicate = seen.has(item.item_name);
+          seen.add(item.item_name);
+          return !isDuplicate;
+        })
+        .slice(0, 10);
 
-        <div className="item-container">
-            {ItemContainer.slice(0,6).map((item, index)=>{
-             return (
-                <div className="item-div"  key={index}>
-                    <img className="item-photos" src={item.img_url}/>
-                    <p>{item.item_name}</p>
-                    <p>£{item.price}</p>
-                </div> )
-            })}
-            
+      setItems(uniqueItems);
+    });
+  }, []);
+
+  return (
+    <div className="item-container">
+      {items.map((item, index) => (
+        <div className="item-div" key={index}>
+          <img
+            className="item-photos"
+            src={item.img_url}
+            alt={item.item_name}
+          />
+          <p>{item.item_name}</p>
+          <p>£{item.price}</p>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default ItemContainer
+export default ItemContainer;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { getUsers } from "../../utils/api";
-import "./Profile.css";
 
 function Profile() {
   const [user, setUser] = useState([]);
@@ -9,22 +8,14 @@ function Profile() {
 
   useEffect(() => {
     getUsers().then((data) => {
-      console.log(data);
       return setUser(data);
     });
   }, []);
 
   return (
     <div>
-      {user.slice(2, 3).map((user, index) => {
-        return (
-          <div key={index}>
-            <img id="profile-pic" src={user.avatar_url} />
-            <p>Username: {user.username}</p>
-            <p>Kudos: {user.kudos}</p>
-          </div>
-        );
-      })}
+      <h1>Profile Page</h1>
+      <p>{user.username}</p>
     </div>
   );
 }
