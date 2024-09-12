1aafdaa4a0bd92d69b3ef94e4a8f220b501a1f78
jasonic69 <justyhill3@hotmail.com>
2024-09-12 20:31:42 +0100

    added buttons to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,23 +5,10 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div>
-      <h2>
-        {" "}
-        <Link to="/">Home</Link>
-      </h2>
-      <h2>
-        {" "}
-        <Link to="/favourites">Favourites</Link>
-      </h2>
-
-      <h2>
-        {" "}
-        <Link to="/basket">Basket</Link>
-      </h2>
-      <h2>
-        {" "}
-        <Link to="/profile">Profile</Link>
-      </h2>
+      <Link to="/"><button className="btn" to="/"><i className="fa fa-home"></i></button></Link>
+      <Link to="/favourites"><button className="btn"><i className="fa fa-heart"></i></button></Link>
+      <Link to="/basket"><button className="btn"><i className="fa fa-shopping-basket"></i></button></Link>
+      <Link to="/profile"><button className="btn"><i className="fa fa-user"></i></button></Link>
     </div>
   );
 }
